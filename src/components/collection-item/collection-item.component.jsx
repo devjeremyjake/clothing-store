82acd200eb1924ecc85cf38f37cc2d312797413b
devjeremyjake <devjeremyjake@gmail.com>
2020-12-24 10:42:28 +0100

    add item to cart redux functionality

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+	const { name, price, imageUrl } = item;
 	return (
 		<div className="collection-item">
 			<div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -11,9 +14,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
 				<span className="name">{name}</span>
 				<span className="price">${price}</span>
 			</div>
-			<CustomButton inverted>Add To Cart</CustomButton>
+			<CustomButton inverted onClick={() => addItem(item)}>
+				Add To Cart
+			</CustomButton>
 		</div>
 	);
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+	addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/preview-collection/preview.component.jsx
+++ src/components/preview-collection/preview.component.jsx
@@ -4,14 +4,16 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import './preview.style.scss';
 
 const Preview = ({ title, items }) => (
-			<div className="collection-preview">
+	<div className="collection-preview">
 		<h1 className="title">{title.toUpperCase()}</h1>
 		<div className="preview">
-			{items.filter((item, idx) => idx < 4).map(({id, ...itemProps}) => (
-				< CollectionItem key={id} {...itemProps}/>
-			))}
+			{items
+				.filter((item, idx) => idx < 4)
+				.map((item) => (
+					<CollectionItem key={item.id} item={item} />
+				))}
 		</div>
-	</div>	
+	</div>
 );
 
 export default Preview;
